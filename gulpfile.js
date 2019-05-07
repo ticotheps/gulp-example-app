dae94cffa59dd24d95ec50cc14800ae750f009be
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-07 12:17:15 -0400

    Creates an example to show how gulp optimizes .scss files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
 
 /* 
     --Top Level Functions--
